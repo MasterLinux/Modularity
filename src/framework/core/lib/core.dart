b03155df220b59c307f0db2200e4bdf2b9a4c2e9
Christoph Grundmann <christoph.grundmann@gmail.com>
2014-02-01 14:45:11 +0100

    initial commit for core library

---
+++ src/framework/core/lib/core.dart
@@ -0,0 +1,12 @@
+library lib.core;
+
+part 'application.dart';
+part 'page.dart';
+part 'fragment.dart';
+part 'module.dart';
+
+class Core {
+  Core() {
+
+  }
+}
---
+++ src/framework/core/lib/fragment.dart
@@ -0,0 +1,6 @@
+part of lib.core;
+
+class Fragment {
+  Fragment() {
+  }
+}
