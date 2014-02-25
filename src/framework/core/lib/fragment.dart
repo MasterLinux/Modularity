e5cdf92b90ddf0a9040231334a2f1618da83ee6c
Christoph Grundmann <christoph.grundmann@gmail.com>
2014-02-25 14:41:47 +0100

    fragment implemented

--- src/framework/core/lib/fragment.dart
+++ src/framework/core/lib/fragment.dart
@@ -9,17 +9,28 @@ class Fragment {
    */
   final String ID_PREFIX = "fragment";
 
+  /**
+   * Initializes the fragment.
+   */
   Fragment() {
     _id = new UniqueId(ID_PREFIX).build();
     _modules = [];
   }
 
-  //TODO implement ID getter
+  /**
+   * Gets the unique ID of the fragment.
+   * This ID is used as node ID.
+   */
+  String get id => _id;
 
-  void _loadModules(List<ConfigModuleModel> moduleConfig) {
+  /**
+   * Loads all modules with the help
+   * of the config.
+   */
+  void _loadModules(List<ConfigModuleModel> modules) {
 
     //creates all modules of this fragment
-    for(var model in moduleConfig) {
+    for(var model in modules) {
       _modules.add(
           new AnnotatedModule(
             model.libraryName,
@@ -28,6 +39,51 @@ class Fragment {
             model.config
           )
       );
+    }
+  }
+
+  /**
+   * Adds the fragment and all its
+   * modules to DOM.
+   */
+  void add() {
+    //TODO add fragment template
+
+    for(var module in _modules) {
+      module.add();
+    }
+  }
+
+  /**
+   * Remove the fragment and all its
+   * modules from DOM.
+   */
+  void remove() {
+    for(var module in _modules) {
+      module.remove();
+    }
+
+    //TODO remove fragment template
+  }
+
+  /**
+   * This event handler is invoked whenever a request
+   * is completed in case the request was successful but
+   * also when an error occurred.
+   */
+  void onRequestCompleted(RequestCompletedEventArgs args) {
+    //TODO allow to catch this event to manipulate fragment -> via config? or via callback?
+
+    for(var module in _modules) {
+      module.onRequestCompleted(args);
+    }
+  }
+
+  void onLoadingStateChanged(LoadingStateChangedEventArgs args) {
+    //TODO allow to catch this event to manipulate fragment
+
+    for(var module in _modules) {
+      module.onLoadingStateChanged(args);
     }
   }
 }
