a0476c6e6f62d411ab4f66393dbe1bb0f05ff7af
Christoph Grundmann <christoph.grundmann@gmail.com>
2014-04-06 18:12:14 +0200

    tab bar prototype updated

--- src/framework/ui/lib/tab_bar/publicize_tab_bar.dart
+++ src/framework/ui/lib/tab_bar/publicize_tab_bar.dart
@@ -3,47 +3,59 @@ library publizice.ui.tab_bar;
 import 'package:polymer/polymer.dart';
 import 'dart:html';
 
+import 'publicize_tab_bar_item.dart';
+
 @CustomTag('publicize-tab-bar')
 class PublicizeTabBar extends PolymerElement {
   @published String onSelectionChanged;
 
-  /**
-   * Local name of a tab bar item element
-   */
-  static const String _ELEMENT_TAB_BAR_ITEM = 'publicize-tab-bar-item';
-
-  /**
-   * Index attribute
-   */
-  static const String _ATTRIBUTE_INDEX = 'index';
-
-  static const String _ATTRIBUTE_SELECTED_INDEX = 'selectedindex';
+  static const String ATTRIBUTE_SELECTED_INDEX = 'selectedindex';
 
   /**
    * ID of the content element which contains
    * all tab bar items
    */
-  static final String _ID_CONTENT = 'items';
+  static const String _ID_CONTENT = 'items';
+
+  /**
+   * Default index of the initial selected item
+   */
+  static const String _INDEX_DEFAULT = '0';
+
+  /**
+   * The local name od this element
+   */
+  static const String ELEMENT_NAME = 'publicize-tab-bar';
 
   /**
    * Initializes the tab bar
    */
   PublicizeTabBar.created() : super.created() {
+    var selectedIndex = _selectedIndex;
+
     //initializes all items
     for(var idx=0; idx<items.length; idx++) {
-      items[idx].attributes[_ATTRIBUTE_INDEX] = idx.toString();
+      var item = items[idx];
+
+      item.attributes[PublicizeTabBarItem.ATTRIBUTE_INDEX] = idx.toString();
+
+      if(selectedIndex == idx) {
+        item.attributes[PublicizeTabBarItem.ATTRIBUTE_SELECTED] = 'true';
+      }
     }
   }
 
   void attributeChanged(String name, String oldValue, String newValue) {
     switch(name) {
-      case _ATTRIBUTE_SELECTED_INDEX:
+      case ATTRIBUTE_SELECTED_INDEX:
+
+        //get previous selected item
         var el = items.firstWhere((e) {
-          return e.attributes[_ATTRIBUTE_INDEX] == oldValue;
+          return e.attributes[PublicizeTabBarItem.ATTRIBUTE_INDEX] == oldValue;
         });
 
         if(el != null) {
-          el.attributes.remove('selected');
+          el.attributes.remove(PublicizeTabBarItem.ATTRIBUTE_SELECTED);
         }
         break;
     }
@@ -56,8 +68,17 @@ class PublicizeTabBar extends PolymerElement {
     var nodes = (this.$[_ID_CONTENT] as ContentElement).getDistributedNodes();
 
     return nodes.where((e) {
-      return e != null && e.localName == _ELEMENT_TAB_BAR_ITEM;
+      return e != null && e.localName == PublicizeTabBarItem.ELEMENT_NAME;
     }).toList();
+  }
+
+  /**
+   * Gets the value of the selectedIndex attribute
+   */
+  int get _selectedIndex {
+    var selectedIndexAttr = this.attributes[ATTRIBUTE_SELECTED_INDEX];
+    int selectedIndex = int.parse(selectedIndexAttr != null ? selectedIndexAttr : _INDEX_DEFAULT);
+    return selectedIndex;
   }
 
 }
