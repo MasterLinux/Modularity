c5a02a7df649312abb17f738ab7dbf27ca52ffb6
Christoph Grundmann <christoph.grundmann@gmail.com>
2014-07-06 18:38:46 +0200

    started to implement template parsing

--- src/framework/core/lib/template/template.dart
+++ src/framework/core/lib/template/template.dart
@@ -9,7 +9,52 @@ part 'element_type.dart';
 part 'binding.dart';
 part 'property.dart';
 
+//https://groups.google.com/a/dartlang.org/forum/#!topic/web-ui/OEPXc8KtpVE
 class Template {
-  Template() {
+  final String template;
+
+  Template.from(this.template) {
+    var n = _parse(template);
+  }
+
+  void _parse(String template) {
+    var node = new Element.html(
+        template,
+        validator: new NodeValidatorBuilder()
+          ..allowHtml5()
+    );
+
+
+    return node;
   }
 }
+   /*
+class ModularityValidator extends NodeValidator {
+
+  /*
+  factory ModularityValidator({UriPolicy uriPolicy}) =>
+    new _Html5NodeValidator(uriPolicy: uriPolicy);
+    */
+  bool allowsAttribute(Element element, String attributeName, String value) {
+    return attributeName.startsWith('mod-');;
+  }
+
+
+  bool allowsElement(Element element) {
+    return element.tagName.startsWith('MOD-');
+  }
+
+
+/*
+  // Allow all elements which begin with ‘NG-’.
+  bool allowsElement(Element element) =>
+  element.tagName.startsWith('NG-');
+  // Allow all attributes beginning with ‘NG-’, regardless of element
+  // tag.
+  bool allowsAttribute(
+      Element element, String attributeName, String value) =>
+  attributeName.startsWith('ng-');
+  */
+
+}
+    */
